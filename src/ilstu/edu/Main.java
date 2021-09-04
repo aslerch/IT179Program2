27f7fd90d63f7f1e430464a78dad04cc08e45e80
aslerch <88261471+aslerch@users.noreply.github.com>
2021-09-03 22:20:18 -0500

    completed printPatientInformation method

--- src/ilstu/edu/Main.java
+++ src/ilstu/edu/Main.java
@@ -3,13 +3,17 @@ package ilstu.edu;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /** Driver class for overall program */
 public class Main {
 
     /** global fields/objects */
-    private static final int ADMIT_PATIENT = 1, PRINT_PATIENT_INFORMATION = 2, SUBMIT_PCR_RESULT = 3, DO_ROUNDS = 4, DISCHARGE_PATIENT = 5, EXIT = 6;
+    private static final int ADMIT_PATIENT = 1, PRINT_PATIENT_INFORMATION = 2, SUBMIT_PCR_RESULT = 3,
+            DO_ROUNDS = 4, DISCHARGE_PATIENT = 5, EXIT = 6, INVALID = -1;
+    private static final double LOWEST_LIVABLE_TEMPERATURE = 24, HIGHEST_LIVABLE_TEMPERATURE = 42;
+    private static final int ID_LOWER_BOUND = 0, ID_UPPER_BOUND = 999999;
     private static Scanner keyboard = new Scanner(System.in);
     private static ArrayList<Patient> allPatients = new ArrayList<Patient>();
 
@@ -21,11 +25,13 @@ public class Main {
         while (menuSelection != EXIT) {
             displayMenu();
             menuSelection = askForIntBetween("Your selection (1-6): ", 1, 6);
-            if ( menuSelection == ADMIT_PATIENT )
+            if (menuSelection == ADMIT_PATIENT) {
                 admitPatient();
-            if ( menuSelection == PRINT_PATIENT_INFORMATION ) {
-                askForIntBetween("")
+                System.out.println("****************************************\n");
+            }
+            if (menuSelection == PRINT_PATIENT_INFORMATION) {
                 printPatientInformation();
+                System.out.println("****************************************\n");
             }
         }
 
@@ -37,7 +43,7 @@ public class Main {
      * @param temperature the patient's temperature
      */
     private static void admitCovidPatient(double temperature) {
-        int id = askForIntBetween("Please enter the patient's id number: ", 0, 999999);
+        int id = askForPatientIdNumber();
         String fName = askForStringWithinLength("Please enter the patient's first name: ", 50);
         String lName = askForStringWithinLength("Please enter the patient's last name: ", 50);
         int age = askForIntBetween("Please enter the patient's age (0-120): ", 0, 120);
@@ -51,7 +57,8 @@ public class Main {
     private static void admitPatient() {
         boolean hasCovid = askForPCRTestResult();
         if ( hasCovid ) {
-            double temperature = askForDoubleBetween(0, 200);
+            double temperature = askForDoubleBetween("Please enter the patient's temperature in celsius (24-42): ",
+                    LOWEST_LIVABLE_TEMPERATURE, HIGHEST_LIVABLE_TEMPERATURE);
             admitCovidPatient(temperature);
         }
         if ( ! hasCovid ) {
@@ -65,7 +72,7 @@ public class Main {
      * @param mainSymptom the patient's main symptom
      */
     private static void admitRegularPatient(String mainSymptom) {
-        int id = askForIntBetween("Please enter the patient's id number: ", 0, 999999);
+        int id = askForPatientIdNumber();
         String fName = askForStringWithinLength("Please enter the patient's first name: ", 50);
         String lName = askForStringWithinLength("Please enter the patient's last name: ", 50);
         int age = askForIntBetween("Please enter the patient's age (0-120): ", 0, 120);
@@ -79,7 +86,7 @@ public class Main {
      * @param num2 The upper bound
      * @return A double between or equal to the upper bound and lower bound
      */
-    private static double askForDoubleBetween(double num1, double num2) {
+    private static double askForDoubleBetween(String informationRequestMessage, double num1, double num2) {
         double input = -9999;
         boolean isDouble = false;
         boolean isBetweenNum1AndNum2 = false;
@@ -87,16 +94,16 @@ public class Main {
             isDouble = false;
             isBetweenNum1AndNum2 = false;
             try {
-                System.out.print("Your input (" + num1 + " - " + num2 + "): ");
+                System.out.print(informationRequestMessage);
                 input = keyboard.nextDouble();
                 if (input >= num1 & input <= num2)
                     isBetweenNum1AndNum2 = true;
                 if (input < num1 || input > num2)
-                    System.out.println("input is outside of acceptable range");
+                    System.out.println("input outside of acceptable range");
                 isDouble = true;
             } catch (InputMismatchException e) {
                 keyboard.nextLine();
-                System.out.println("Incorrect input: not a double");
+                System.out.println("Incorrect input: not a double value");
             }
         }
         return input;
@@ -116,7 +123,7 @@ public class Main {
             isInt = false;
             isBetweenNum1AndNum2 = false;
             try {
-                System.out.print();
+                System.out.print(informationRequestMessage);
                 input = keyboard.nextInt();
                 if (input >= num1 & input <= num2)
                     isBetweenNum1AndNum2 = true;
@@ -128,7 +135,24 @@ public class Main {
                 System.out.println("Incorrect input: not an integer");
             }
         }
+        keyboard.nextLine();
         return input;
+    }
+
+    /**
+     * asks the user for a patient id number
+     * @return patient id number if the patient exists, -1(INVALID) if the patient does not exist
+     */
+    private static int askForPatientIdNumber() {
+        int id = INVALID;
+        while (id == INVALID) {
+            id = askForIntBetween("Please enter an id number for the patient: ", ID_LOWER_BOUND, ID_UPPER_BOUND);
+            if ( doesPatientExist(id) == true ) {
+                System.out.println("That id number is already taken");
+                id = INVALID;
+            }
+        }
+        return id;
     }
 
     /**
@@ -139,7 +163,8 @@ public class Main {
         String informationRequestMessage = """
                 What was the result of the PCR Test?
                 1. Positive
-                2. Negative""";
+                2. Negative
+                Your selection (1-2):  """;
         int selection = askForIntBetween(informationRequestMessage, 1, 2);
         boolean hasCovid = true;
         if ( selection == 1)
@@ -156,7 +181,6 @@ public class Main {
      * @return A String within a length determined by the upper bound
      */
     private static String askForStringWithinLength(String informationRequestMessage, int lengthUpperBound) {
-        keyboard.nextLine();
         boolean isWithinCorrectLength = false;
         String output = "";
         while (isWithinCorrectLength == false) {
@@ -191,6 +215,48 @@ public class Main {
                 ****************************************
                 Welcome to the Hospital Patient Database
                 ****************************************""");
+    }
+
+    /**
+     * checks to see if the patient exists in the allPatients ArrayList
+     * @param id identification number
+     * @return true if patient exists, false if patient does not exist
+     */
+    private static boolean doesPatientExist(int id) {
+        boolean doesExist = false;
+        if ( searchAllPatientsFor(id) != null)
+            doesExist = true;
+        return doesExist;
+    }
+
+    /**
+     * prints the patient's information if they exist
+     */
+    private static void printPatientInformation() {
+        int returnId = INVALID;
+        int id = askForIntBetween("Please enter the patient's id number: ",
+                ID_LOWER_BOUND, ID_UPPER_BOUND);
+        boolean patientExists = doesPatientExist(id);
+        if (patientExists)
+            returnId = id;
+        if (returnId != INVALID)
+            System.out.println(searchAllPatientsFor(id).toString());
+        if (returnId == INVALID)
+            System.out.println("no patient has that id number");
+    }
+
+    /**
+     * searches the allPatients ArrayList for a patient based on their id number
+     * @param id identification number
+     * @return the patient if they exist, null if they do not exist
+     */
+    private static Patient searchAllPatientsFor(int id) {
+        Patient returnPatient = null;
+        for (Patient patient : allPatients) {
+            if (patient.getId() == id)
+                returnPatient = patient;
+        }
+        return returnPatient;
     }
 
 
